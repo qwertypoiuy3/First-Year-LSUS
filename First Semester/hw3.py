f082e57496400d6ece8af613ed2cd734e6df8e0a
Toro45 <49995487+qwertypoiuy3@users.noreply.github.com>
2024-08-16 20:46:30 -0500

    Update hw3.py

--- First Semester/hw3.py
+++ First Semester/hw3.py
@@ -1,5 +1,9 @@
 #Karin Galicia
 #Hw 3
+#Input1: Starting weight of food in lbs:oz.
+#Input2: Ending weight of food in lbs:oz.
+#Convert lbs to oz from input 1 and 2.
+#Substract input 1 and 2
 
 startingWeight = input("Enter the starting weight of the food: ")
 find = startingWeight.find(":")
